4d28a56c5cc146f9915cf9c607ffed412dcf55a7
BlancJorry <102340354+BlancJorry@users.noreply.github.com>
2023-08-12 19:21:12 -0400

    Update bat.py

--- Battle/bat.py
+++ Battle/bat.py
@@ -6,7 +6,7 @@ vale1 = input("entre yon karakte")
 for i in vale1:
     if i.isdigit():
         s=s+1
-        tab2 = i
+        tab2.append(i)
 print("kantite dijit nan fraz ou a se :",s) 
 print('digit yo se:',tab2)
         
