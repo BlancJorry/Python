5d9bbe63b6640e0db969bd05e1b86e9df81e575d
BlancJorry <102340354+BlancJorry@users.noreply.github.com>
2023-08-19 17:07:19 -0400

    Add files via upload

---
+++ 19_battle/battle.py
@@ -0,0 +1,19 @@
+
+lis = [1,2,3,4,5]
+
+def pg (l):
+    t = l[0]
+    for i in l:
+       if i > t:
+           t=l[i]
+    print('pigran c',t)    
+pg(lis)    
+
+
+def pt (l):
+    t = l[0]
+    for i in l:
+       if i < t:
+           t=l[i]
+    print('pigran c',t) 
+pt(lis)    
